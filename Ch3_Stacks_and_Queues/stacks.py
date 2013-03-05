c156914270a9c46a5880b88c48d3b2ca8b7a6dd7
Keith Mascarenhas <keithxm23@gmail.com>
2013-03-05 17:52:12 -0500

    wrote basic stack using linked list

---
+++ Ch3_Stacks_and_Queues/stacks.py
@@ -0,0 +1,43 @@
+#stack
+
+class Stack():
+	
+	def __init__(self):
+		self.top = None
+
+	class Node():
+		def __init__(self, data):
+			self.data = data
+			self.next = None
+			
+
+	def pop(self):
+		if self.top == None:
+			raise Exception("Stack Underflow")
+		temp = self.top
+		self.top = self.top.next
+		return temp
+
+
+	def push(self, data):
+		new = self.Node(data)
+		new.next = self.top
+		self.top = new
+
+	def peek(self):
+		if self.top == None:
+			raise Exception("Stack empty")
+		return self.top.data
+
+
+#Test
+s = Stack()
+#s.pop()
+
+for x in xrange(1,10):
+	s.push(x)
+
+print s.peek()
+x = s.pop()
+print x.data
+print s.peek()
